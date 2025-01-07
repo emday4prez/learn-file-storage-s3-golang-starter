32156bb08682cc8f7735f451df93648212a5c35b
Emerson <emerson.day@gmail.com>
2025-01-06 21:54:05 -0800

    pass tests for img upload

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -30,7 +31,43 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
-	// how to upload files in go
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	ct := header.Header.Get("Content-Type")
+	fmt.Printf("content type: %v\n", ct)
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to read image data", err)
+		return
+	}
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to get video metadata", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "user id does not match video owner", err)
+		return
+	}
+
+	videoThumbStruct := thumbnail{
+		data:      imageData,
+		mediaType: ct,
+	}
+
+	videoThumbnails[videoID] = videoThumbStruct
+
+	url := fmt.Sprintf("http://localhost:%d/api/thumbnails/%s", 8091, videoID)
+	video.ThumbnailURL = &url
+
+	cfg.db.UpdateVideo(video)
+	respondWithJSON(w, http.StatusOK, video)
 }
